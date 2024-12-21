c74fb5013d8fc9545bf0ec543a76f50fad78d4ab
mannuel25 <oluwasegunprosperity@gmail.com>
2024-12-21 21:14:57 +0100

    first commit..print to screen

---
+++ Part I/gotest.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func displayGRreetings(){
+	fmt.Printf("Hello, Emmanuel 👋")
+}
+
+func main() {
+	fmt.Println("Hello, world!")
+	displayGRreetings()
+}
+
+/*
+	ways to run the file: (1) complile and run --> go build gotest.go, then gotest, (2) run directly --> go run gotest.go
+*/
