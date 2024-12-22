929e8534cc8163cdbd212b223d05fdae9e49a240
mannuel25 <oluwasegunprosperity@gmail.com>
2024-12-22 17:29:50 +0100

    variable declarations, statements

--- Part I/gotest.go
+++ Part I/gotest.go
@@ -1,15 +1,43 @@
 package main
+
 import "fmt"
 
-func displayGRreetings(){
-	fmt.Printf("Hello, Emmanuel 👋")
+func displayGrreetings() {
+	fmt.Printf("Hello, Emmanuel 👋\n")
 }
+
+// single-line function
+func message() { fmt.Println("Hello!"); fmt.Println("My name is John."); };
+
+func loopFunc(){
+	ctr := 0
+	for ctr < 10 {
+		fmt.Println("ctr:", ctr)
+		ctr +=1
+	}
+}
+
+var name string = "Emmanuel"
+var is_cold = false
 
 func main() {
 	fmt.Println("Hello, world!")
-	displayGRreetings()
+	displayGrreetings()
+	message()
+	loopFunc()
+	fmt.Println("My name is", name)
+
+	age := 10
+	fmt.Println("Age: ", age)
+	fmt.Println("is_cold: ", is_cold)
+	amount, gender := 25, "male"
+	var decimal, number float32 = 2.2222222, 1.0090
+	fmt.Println(amount, gender, number, decimal)
 }
 
 /*
 	ways to run the file: (1) complile and run --> go build gotest.go, then gotest, (2) run directly --> go run gotest.go
+	to format my codes --> go fmt gotest.go
+	:= --> short variable declaration, var age int = 10 --> explicit declaration
 */
+
