d73dfdd2e6f4ea8a6898ce5b0ec8283a02a551e5
mannuel25 <oluwasegunprosperity@gmail.com>
2024-12-23 14:30:07 +0100

    basic arithmetics

--- Part I/gotest.go
+++ Part I/gotest.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func displayGrreetings() {
 	fmt.Printf("Hello, Emmanuel 👋\n")
@@ -17,6 +20,7 @@ func loopFunc(){
 	}
 }
 
+
 func getUserInfo(){
 	print("What's your name?: ")
 	var my_name string
@@ -28,8 +32,40 @@ func getUserInfo(){
 	fmt.Scan(&amount)
 	amount_to_float := float64(amount)
 	fmt.Print(amount, amount_to_float)
+	fmt.Println("\nEnter your first and last name: ")
+	var first_name, last_name string
+	fmt.Scan(&first_name, &last_name)
+	message := fmt.Sprintf("Your first name is %s and your last name is %s", first_name, last_name)
+	fmt.Println(message)
+	/* 
+		fmt.Scan reads space-separated values from input. It stops reading when it encounters a space, tab, or newline.
+		fmt.Scanln reads input until it encounters a newline (\n) character.
+	*/
+	full_name := first_name + " " + last_name
+	fmt.Println("Full Name: ", full_name)
 
 
+}
+
+
+func arithmetic(){
+	var first_number, second_number string
+	fmt.Println("Enter two numbers: ")
+	fmt.Scan(&first_number, &second_number)
+	int_first_number, _ := strconv.Atoi(first_number)
+	int_second_number, _ := strconv.Atoi(second_number)
+	addition := int_first_number + int_second_number
+	addition_msg := fmt.Sprintf("Addition of %d and %d is %d", int_first_number, int_second_number, addition)
+	subtraction := int_first_number - int_second_number
+	subtraction_msg := fmt.Sprintf("Subtraction of %d and %d is %d", int_first_number, int_second_number, subtraction)
+	division := int_first_number / int_second_number
+	division_msg := fmt.Sprintf("Division of %d and %d is %d", int_first_number, int_second_number, division)
+	multiplication := int_first_number * int_second_number
+	multiplication_msg := fmt.Sprintf("Multiplication of %d and %d is %d", int_first_number, int_second_number, multiplication)
+	fmt.Println(addition_msg)
+	fmt.Println(subtraction_msg)
+	fmt.Println(multiplication_msg)
+	fmt.Println(division_msg)
 }
 
 // static-typed variable
@@ -38,22 +74,23 @@ var is_cold = false
 
 func main() {
 	fmt.Println("Hello, world!")
-	displayGrreetings()
-	message()
-	loopFunc()
+	// displayGrreetings()
+	// message()
+	// loopFunc()
 	fmt.Println("My name is", name)
 
 	// dynamic-typed variable
-	age := 10
-	fmt.Println("Age: ", age)
-	fmt.Println("is_cold: ", is_cold)
-	amount, gender := 25, "male"
-	var decimal, number float32 = 2.2222222, 1.0090
-	fmt.Println(amount, gender, number, decimal)
-	message := fmt.Sprintf("The amount is %d and the decimal is %f", amount, decimal)
-	fmt.Println(message)
-	fmt.Println("Memory Address of message is", &message)
-	getUserInfo()
+	// age := 10
+	// fmt.Println("Age: ", age)
+	// fmt.Println("is_cold: ", is_cold)
+	// amount, gender := 25, "male"
+	// var decimal, number float32 = 2.2222222, 1.0090
+	// fmt.Println(amount, gender, number, decimal)
+	// message := fmt.Sprintf("The amount is %d and the decimal is %f", amount, decimal)
+	// fmt.Println(message)
+	// fmt.Println("Memory Address of message is", &message)
+	// getUserInfo()
+	arithmetic()
 }
 
 /*
