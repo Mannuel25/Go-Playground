24e6e640ad2447c39c5e08ddd985ffb6d909fd59
mannuel25 <oluwasegunprosperity@gmail.com>
2024-12-28 23:55:46 +0100

    functions..

--- Part II/code_lab.go
+++ Part II/code_lab.go
@@ -28,14 +28,34 @@ func sumN(numbers ... int){
 	fmt.Printf("The sum of all numbers is: %v\n", sum)
 }
 
+func factorial(n int) int {
+    if n == 0 {
+        return 1
+    }
+	fmt.Println("...", n * factorial(n- 1))
+    return n * factorial(n- 1)
+}
+
 func main() {
-	a := "elizabeth"
-	DisplayUpper(a)
-	area, perimeter := rectangleOperations(2.35,9.22)
-	fmt.Println("area:", area)
-	fmt.Println("perimeter:", perimeter)
-	fmt.Println("Hey!")
-	sumN(2,3,3,4,221,5,5)
-	sumN(2,19,929,12234,2,4,24)
+	// a := "elizabeth"
+	// DisplayUpper(a)
+	// area, perimeter := rectangleOperations(2.35,9.22)
+	// fmt.Println("area:", area)
+	// fmt.Println("perimeter:", perimeter)
+	// fmt.Println("Hey!")
+	// sumN(2,3,3,4,221,5,5)
+	// sumN(2,19,929,12234,2,4,24)
+	// b := 5
+    // fmt.Println(factorial(b))
+
+	// using a function as a value
+	sumNums := func() int {
+		nums, sum := [2]int{2,9}, 0
+		for _, value := range(nums) {
+			sum += value
+		}
+		return sum
+	}
+	fmt.Println("The sum of both numbers is",sumNums())
 }
 
